30ed41abe770a3b1215f58bcb7ef47ee4bbc600d
Simeon Jackman <simeon@jackman.ch>
2025-12-29 16:45:58 +0000

    CHANGE: json loading

--- src/workout.js
+++ src/workout.js
@@ -1,5 +1,5 @@
 import ex from "./assets/exercises.json";
 import fm from "./assets/formats.json";
 
-export var  exercises = ex.exercises;
-export var formats = fm.formats;
+export var  exercises = ex;
+export var formats = fm;
